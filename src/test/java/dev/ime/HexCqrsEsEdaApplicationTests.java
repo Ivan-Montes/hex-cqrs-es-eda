9c4e68b10210826e8f2f7762ab192cc04a27fc06
IvanM <ivan.montes@outlook.com>
2024-06-08 08:20:15 +0200

    feat: initial commit

---
+++ src/test/java/dev/ime/HexCqrsEsEdaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexCqrsEsEdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
