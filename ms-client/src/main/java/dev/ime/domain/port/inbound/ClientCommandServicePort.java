b24d119895fe38153efa708c9f582c1590b65e73
IvanM <ivan.montes@outlook.com>
2024-06-10 07:54:34 +0200

    feat: add client command service

---
+++ ms-client/src/main/java/dev/ime/domain/port/inbound/ClientCommandServicePort.java
@@ -0,0 +1,15 @@
+package dev.ime.domain.port.inbound;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import dev.ime.domain.event.Event;
+
+public interface ClientCommandServicePort<T> {
+	
+	Optional<Event> create(T dto);
+	Optional<Event> update(UUID id, T dto);
+	Optional<Event> deleteById(UUID id);
+	
+}
+
