9f9ec094b552bbaeda0ae6f0d7b95d97e25e5b2f
IvanM <ivan.montes@outlook.com>
2024-06-11 07:56:28 +0200

    feat: add db repository ports

---
+++ ms-client/src/main/java/dev/ime/domain/port/outbound/ClientNoSqlWriteRepositoryPort.java
@@ -0,0 +1,11 @@
+package dev.ime.domain.port.outbound;
+
+import java.util.Optional;
+
+import dev.ime.domain.event.Event;
+
+public interface ClientNoSqlWriteRepositoryPort {
+	
+	Optional<Event> save(Event event);
+
+}
