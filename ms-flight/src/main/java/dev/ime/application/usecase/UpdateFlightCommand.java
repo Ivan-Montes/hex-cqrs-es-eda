e6fb831831ba0a2ad222b929b676829bc53b35b2
IvanM <ivan.montes@outlook.com>
2024-06-18 07:46:49 +0200

    feat: add commands impl

---
+++ ms-flight/src/main/java/dev/ime/application/usecase/UpdateFlightCommand.java
@@ -0,0 +1,18 @@
+package dev.ime.application.usecase;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+import dev.ime.domain.command.Command;
+
+public record UpdateFlightCommand(
+		UUID flightId,
+		String origin,
+		String destiny,
+		LocalDate departureDate,
+		LocalTime departureTime,
+		UUID planeId
+		) implements Command {
+
+}
