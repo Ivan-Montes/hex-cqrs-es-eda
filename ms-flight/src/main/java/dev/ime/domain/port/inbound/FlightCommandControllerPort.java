e4e867322824ff837a2431a7d82ee37e1b8f94b5
IvanM <ivan.montes@outlook.com>
2024-06-19 07:54:16 +0200

    feat: add controller port

---
+++ ms-flight/src/main/java/dev/ime/domain/port/inbound/FlightCommandControllerPort.java
@@ -0,0 +1,15 @@
+package dev.ime.domain.port.inbound;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+
+import dev.ime.domain.event.Event;
+
+public interface FlightCommandControllerPort<T> {
+
+	ResponseEntity<Event>create(T dto);
+	ResponseEntity<Event>update(UUID id, T dto);
+	ResponseEntity<Event>deleteById(UUID id);
+	
+}
