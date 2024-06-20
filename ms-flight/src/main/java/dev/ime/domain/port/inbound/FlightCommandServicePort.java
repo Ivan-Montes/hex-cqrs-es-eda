551fe7842f84e83fea4275e0e83dc4910f456615
IvanM <ivan.montes@outlook.com>
2024-06-20 07:53:24 +0200

    feat: add service port

---
+++ ms-flight/src/main/java/dev/ime/domain/port/inbound/FlightCommandServicePort.java
@@ -0,0 +1,14 @@
+package dev.ime.domain.port.inbound;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import dev.ime.domain.event.Event;
+
+public interface FlightCommandServicePort<T> {
+
+	Optional<Event> create(T dto);
+	Optional<Event> update(UUID id, T dto);
+	Optional<Event> deleteById(UUID id);
+	
+}
