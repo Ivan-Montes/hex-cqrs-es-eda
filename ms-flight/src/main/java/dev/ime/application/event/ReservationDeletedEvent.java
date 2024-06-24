a3c6afe48fb4fc9ab36f4f86db5962fafba00e2b
IvanM <ivan.montes@outlook.com>
2024-06-24 07:56:49 +0200

    feat: add events

---
+++ ms-flight/src/main/java/dev/ime/application/event/ReservationDeletedEvent.java
@@ -0,0 +1,32 @@
+package dev.ime.application.event;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import dev.ime.application.config.ApplicationConstant;
+import dev.ime.domain.event.Event;
+
+public final class ReservationDeletedEvent extends Event{
+
+	private final UUID reservationId;
+
+	@JsonCreator
+	public ReservationDeletedEvent(@JsonProperty("sequence")Long sequence, @JsonProperty("reservationId")UUID reservationId) {
+		super(ApplicationConstant.CAT_RESERV, ApplicationConstant.RESERVATION_DELETED, sequence);
+		this.reservationId = reservationId;
+	}
+
+	public UUID getReservationId() {
+		return reservationId;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservationDeletedEvent [reservationId=" + reservationId + ", eventId=" + eventId + ", eventCategory="
+				+ eventCategory + ", eventType=" + eventType + ", timeInstant=" + timeInstant + ", sequence=" + sequence
+				+ "]";
+	}	
+	
+}
