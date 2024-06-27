449d28f21bcfad75fb783707f1084116519e6deb
IvanM <ivan.montes@outlook.com>
2024-06-27 07:51:40 +0200

    feat: ms-reservation initial commit

---
+++ ms-reservation/src/main/java/dev/ime/MsReservationApplication.java
@@ -0,0 +1,26 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+@OpenAPIDefinition (info =
+@Info(
+          title = "MsReservationApplication",
+          version = "1.0",
+          description = "hex-cqrs-es-eda project",
+          license = @License(name = "GNU GPLv3", url = "https://choosealicense.com/licenses/gpl-3.0/"),
+          contact = @Contact(url = "https://github.com/Ivan-Montes", name = "IvanM", email = "ivan@github.com")
+  )
+)
+@SpringBootApplication
+public class MsReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReservationApplication.class, args);
+	}
+
+}
