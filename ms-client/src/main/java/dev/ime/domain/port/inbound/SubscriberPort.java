48d4b231d6c18d8924c31afbe868c31f45483420
IvanM <ivan.montes@outlook.com>
2024-06-29 07:57:05 +0200

    refactor: improve naming

--- ms-client/src/main/java/dev/ime/domain/port/inbound/SubscriberPort.java
+++ ms-client/src/main/java/dev/ime/domain/port/inbound/SubscriberPort.java
@@ -2,7 +2,7 @@ package dev.ime.domain.port.inbound;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public interface ClientSubscriberPort {
+public interface SubscriberPort {
 
 	void onMessage(ConsumerRecord<String, Object> consumerRecord);
 	
