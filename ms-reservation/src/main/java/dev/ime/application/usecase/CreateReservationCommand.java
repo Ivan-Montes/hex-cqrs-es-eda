4bf41c0fbbd9f155b68a80cd5922c7b72c39b8a5
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-07-06 07:51:59 +0200

    feat: add command usecases

---
+++ ms-reservation/src/main/java/dev/ime/application/usecase/CreateReservationCommand.java
@@ -0,0 +1,15 @@
+package dev.ime.application.usecase;
+
+import java.util.Set;
+import java.util.UUID;
+
+import dev.ime.domain.command.Command;
+
+public record CreateReservationCommand(
+		UUID reservationId,
+		UUID clientId,
+		UUID flightId,
+		Set<UUID> seatIdsSet
+		) implements Command {
+
+}
