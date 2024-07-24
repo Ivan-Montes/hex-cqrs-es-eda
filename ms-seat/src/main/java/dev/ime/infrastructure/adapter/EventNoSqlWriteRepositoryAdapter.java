1b7099e003778d171c2f37bce3a6f632bf4b51f8
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-07-24 08:21:24 +0200

    feat: add Event repo adapter

---
+++ ms-seat/src/main/java/dev/ime/infrastructure/adapter/EventNoSqlWriteRepositoryAdapter.java
@@ -0,0 +1,28 @@
+package dev.ime.infrastructure.adapter;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import dev.ime.domain.event.Event;
+import dev.ime.domain.port.outbound.EventNoSqlWriteRepositoryPort;
+import dev.ime.infrastructure.repository.write.EventNoSqlWriteRepository;
+
+@Repository
+public class EventNoSqlWriteRepositoryAdapter implements EventNoSqlWriteRepositoryPort{
+
+	private final EventNoSqlWriteRepository eventNoSqlWriteRepository;
+	
+	public EventNoSqlWriteRepositoryAdapter(EventNoSqlWriteRepository eventNoSqlWriteRepository) {
+		super();
+		this.eventNoSqlWriteRepository = eventNoSqlWriteRepository;
+	}
+
+	@Override
+	public Optional<Event> save(Event event) {
+		
+		return Optional.ofNullable(eventNoSqlWriteRepository.save(event));
+		
+	}
+
+}
