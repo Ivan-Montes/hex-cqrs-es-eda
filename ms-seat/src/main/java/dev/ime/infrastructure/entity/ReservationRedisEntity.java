3ba0c8b76ad5cc21fd89a046dbf2ec92b113e72d
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-07-24 08:27:07 +0200

    feat: add Redis entities

---
+++ ms-seat/src/main/java/dev/ime/infrastructure/entity/ReservationRedisEntity.java
@@ -0,0 +1,30 @@
+package dev.ime.infrastructure.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@RedisHash
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ReservationRedisEntity {
+
+	@Id
+	private UUID reservationId;
+	private UUID clientId;
+	private UUID flightId;
+	@Indexed
+	private Set<UUID> seatIdsSet = new HashSet<>();
+	
+}
