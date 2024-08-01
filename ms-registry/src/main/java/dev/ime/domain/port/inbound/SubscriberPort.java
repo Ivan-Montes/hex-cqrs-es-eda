8a9f13ba53974f2a27efbf3cfc6b59914959ce8d
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-01 07:47:45 +0200

    feat: add subscriber port

---
+++ ms-registry/src/main/java/dev/ime/domain/port/inbound/SubscriberPort.java
@@ -0,0 +1,9 @@
+package dev.ime.domain.port.inbound;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface SubscriberPort {
+
+	void onMessage(ConsumerRecord<String, Object> consumerRecord);
+
+}
