0040e0cb3d43d71ff9d1bdfacc612114b84e62e5
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-01 07:48:33 +0200

    feat: add query controller port

---
+++ ms-registry/src/main/java/dev/ime/domain/port/inbound/GenericQueryControllerPort.java
@@ -0,0 +1,13 @@
+package dev.ime.domain.port.inbound;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+
+public interface GenericQueryControllerPort<T> {
+
+	ResponseEntity<List<T>> getAll(Integer page, Integer size);
+	ResponseEntity<T>getById(UUID id);
+	
+}
