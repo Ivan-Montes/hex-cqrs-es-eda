2a8be9af633b89237678429598d2d18dd3a4b7cc
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-03 07:51:02 +0200

    feat: add Payment events

---
+++ ms-registry/src/main/java/dev/ime/application/event/PaymentDeletedEvent.java
@@ -0,0 +1,33 @@
+package dev.ime.application.event;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import dev.ime.application.config.ApplicationConstant;
+import dev.ime.domain.event.Event;
+
+
+public class PaymentDeletedEvent extends Event{
+
+	private final UUID paymentId;
+
+	@JsonCreator
+	public PaymentDeletedEvent(@JsonProperty("sequence")Long sequence, @JsonProperty("paymentId")UUID paymentId) {
+		super(ApplicationConstant.CAT_PAY, ApplicationConstant.PAYMENT_DELETED, sequence);
+		this.paymentId = paymentId;
+	}
+
+	public UUID getPaymentId() {
+		return paymentId;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentDeletedEvent [paymentId=" + paymentId + ", eventId=" + eventId + ", eventCategory="
+				+ eventCategory + ", eventType=" + eventType + ", timeInstant=" + timeInstant + ", sequence=" + sequence
+				+ "]";
+	}
+	
+}
---
+++ ms-registry/src/main/java/dev/ime/application/event/PaymentUpdatedEvent.java
@@ -0,0 +1,52 @@
+package dev.ime.application.event;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import dev.ime.application.config.ApplicationConstant;
+import dev.ime.domain.event.Event;
+
+
+public class PaymentUpdatedEvent extends Event{
+
+	private final UUID paymentId;
+	private final UUID reservationId;
+	private final UUID clientId;
+	private final UUID flightId;
+
+	@JsonCreator
+	public PaymentUpdatedEvent(@JsonProperty("sequence")Long sequence, @JsonProperty("paymentId")UUID paymentId,
+			@JsonProperty("reservationId")UUID reservationId, @JsonProperty("clientId")UUID clientId, @JsonProperty("flightId")UUID flightId) {
+		super(ApplicationConstant.CAT_PAY, ApplicationConstant.PAYMENT_UPDATED, sequence);
+		this.paymentId = paymentId;
+		this.reservationId = reservationId;
+		this.clientId = clientId;
+		this.flightId = flightId;
+	}
+
+	public UUID getPaymentId() {
+		return paymentId;
+	}
+
+	public UUID getReservationId() {
+		return reservationId;
+	}
+
+	public UUID getClientId() {
+		return clientId;
+	}
+
+	public UUID getFlightId() {
+		return flightId;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentUpdatedEvent [paymentId=" + paymentId + ", reservationId=" + reservationId + ", clientId="
+				+ clientId + ", flightId=" + flightId + ", eventId=" + eventId + ", eventCategory=" + eventCategory
+				+ ", eventType=" + eventType + ", timeInstant=" + timeInstant + ", sequence=" + sequence + "]";
+	}
+	
+}
