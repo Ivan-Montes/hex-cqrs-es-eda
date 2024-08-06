bbf0a3a4c1e3a776d4d68d0d9af941efbed2694c
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-06 08:17:55 +0200

    feat: add Kafka config

---
+++ ms-registry/src/main/java/dev/ime/infrastructure/config/KafkaConfig.java
@@ -0,0 +1,22 @@
+package dev.ime.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+
+import dev.ime.application.config.ApplicationConstant;
+
+@Configuration
+public class KafkaConfig {
+	
+	@Bean
+	KafkaAdmin.NewTopics topics() {
+		
+	    return new NewTopics(
+	            TopicBuilder.name(ApplicationConstant.REGISTRY_CREATED).build()
+	            );
+	}
+	
+}
