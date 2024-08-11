b6133b98f78bbf8230d56afb6a188258c8cae772
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-11 09:10:39 +0200

    feat: add for KafkaConfig

---
+++ ms-registry/src/test/java/dev/ime/infrastructure/config/KafkaConfigTest.java
@@ -0,0 +1,27 @@
+package dev.ime.infrastructure.config;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@ExtendWith(MockitoExtension.class)
+class KafkaConfigTest {
+
+	@InjectMocks
+	private KafkaConfig kafkaConfig;
+	
+	@Test
+	void KafkaConfig__topics_ReturnNewTopics() {
+		
+		KafkaAdmin.NewTopics topics = kafkaConfig.topics();
+		
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(topics).isNotNull()
+				);		
+	}
+
+}
