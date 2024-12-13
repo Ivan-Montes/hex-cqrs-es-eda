0700d7013586dc12ad2a6f8c7b6f2eeb244bdec4
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-13 07:36:28 +0100

    fix: improve queries

--- ms-reservation/src/main/java/dev/ime/infrastructure/repository/ReservationJpaRepository.java
+++ ms-reservation/src/main/java/dev/ime/infrastructure/repository/ReservationJpaRepository.java
@@ -13,19 +13,32 @@ import jakarta.persistence.Tuple;
 
 public interface ReservationJpaRepository extends JpaRepository<ReservationJpaEntity, UUID>{
 
-	@Query( value="SELECT r.reservation_id, r.client_id, r.flight_id, s.seat_id FROM reservations r INNER JOIN seats s ON s.reservation_id = r.reservation_id where r.flight_id = :flightId and seat_id in :seatIdsSet ORDER BY r.reservation_id", 
+	@Query( value="SELECT r.reservation_id, r.client_id, r.flight_id, s.seat_id "
+			+ "FROM reservations r "
+			+ "INNER JOIN seats s ON s.reservation_id = r.reservation_id "
+			+ "WHERE r.flight_id = :flightId AND seat_id IN(:seatIdsSet) "
+			+ "ORDER BY r.reservation_id", 
 			nativeQuery=true )
 	List<Tuple> findReservationJpaEntityByFlightIdAndSeatIdSetQueryNative(@Param("flightId")UUID flightId, @Param("seatIdsSet")Collection<UUID> seatIdsSet);
 	
-	@Query( value="SELECT COUNT(r.reservation_id) FROM reservations r INNER JOIN seats s ON s.reservation_id = r.reservation_id where r.flight_id = :flightId and seat_id in :seatIdsSet", 
+	@Query( value="SELECT COUNT(*) "
+			+ "FROM reservations r "
+			+ "INNER JOIN seats s ON s.reservation_id = r.reservation_id "
+			+ "WHERE r.flight_id = :flightId AND seat_id IN(:seatIdsSet)", 
 			nativeQuery=true )
 	int countReservationJpaEntityByFlightIdAndSeatIdSet(@Param("flightId")UUID flightId, @Param("seatIdsSet")Collection<UUID> seatIdsSet);
 	
-	@Query( value="SELECT COUNT(r.reservation_id) FROM reservations r INNER JOIN seats s ON s.reservation_id = r.reservation_id where r.flight_id = :flightId and seat_id in :seatIdsSet and client_id != :clientId", 
+	@Query( value="SELECT COUNT(*) "
+			+ "FROM reservations r "
+			+ "INNER JOIN seats s ON s.reservation_id = r.reservation_id "
+			+ "WHERE r.flight_id = :flightId AND seat_id IN(:seatIdsSet) and client_id != :clientId", 
 			nativeQuery=true )
 	int countReservationJpaEntityByFlightIdSeatIdSetButDifferentClient(@Param("flightId")UUID flightId, @Param("seatIdsSet")Collection<UUID> seatIdsSet, @Param("clientId")UUID clientId);
 
-	@Query( value="SELECT COUNT(r.reservation_id) FROM reservations r INNER JOIN seats s ON s.reservation_id = r.reservation_id where r.reservation_id != :reservationId and r.flight_id = :flightId and seat_id in :seatIdsSet and client_id = :clientId", 
+	@Query( value="SELECT COUNT(*) "
+			+ "FROM reservations r "
+			+ "INNER JOIN seats s ON s.reservation_id = r.reservation_id "
+			+ "WHERE r.reservation_id != :reservationId AND r.flight_id = :flightId and seat_id IN(:seatIdsSet) and client_id = :clientId", 
 			nativeQuery=true )
 	int countReservationJpaEntityByFlightIdSeatIdSetWithSameClientDifferentReservation(@Param("reservationId")UUID reservationId, @Param("flightId")UUID flightId, @Param("seatIdsSet")Collection<UUID> seatIdsSet, @Param("clientId")UUID clientId);
 	
